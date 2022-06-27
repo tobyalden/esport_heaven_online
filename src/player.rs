f7d24f5274c7caf49be5a9688b60f4960c777e12
Toby Alden <toby.alden@gmail.com>
2022-06-27 12:55:34 -0700

    Begin adding player physics

--- src/player.rs
+++ src/player.rs
@@ -2,7 +2,34 @@ use serde::{Deserialize, Serialize};
 
 use crate::game::{INPUT_DOWN, INPUT_LEFT, INPUT_RIGHT, INPUT_UP};
 use crate::level::{Level, TILE_SIZE};
-use crate::utils::{do_hitboxes_overlap, Hitbox, IntVector2D};
+use crate::utils::{approach, clamp, do_hitboxes_overlap, Hitbox, input_check, IntVector2D};
+
+pub const RUN_ACCEL: i32 = 400 * 1000;
+pub const RUN_ACCEL_TURN_MULTIPLIER: i32 = 2;
+pub const RUN_DECEL: i32 = RUN_ACCEL * RUN_ACCEL_TURN_MULTIPLIER;
+//pub const AIR_ACCEL: i32 = 360;
+//pub const AIR_DECEL: i32 = 360;
+pub const MAX_RUN_SPEED: i32 = 100 * 1000;
+//pub const MAX_SUPERJUMP_SPEED_X: i32 = 250;
+//pub const MAX_SUPERJUMP_SPEED_X_OFF_WALL_SLIDE: i32 = 150;
+//pub const MAX_AIR_SPEED: i32 = 120;
+//pub const GRAVITY: i32 = 500;
+//pub const FASTFALL_GRAVITY: i32 = 1200;
+//pub const GRAVITY_ON_WALL: i32 = 150;
+//pub const JUMP_POWER: i32 = 160;
+//pub const JUMP_CANCEL_POWER: i32 = 40;
+//pub const WALL_JUMP_POWER_X: i32 = 130;
+//pub const WALL_JUMP_POWER_Y: i32 = 120;
+//pub const WALL_STICKINESS: i32 = 60;
+//pub const MAX_FALL_SPEED: i32 = 270;
+//pub const MAX_FALL_SPEED_ON_WALL: i32 = 200;
+//pub const MAX_FASTFALL_SPEED: i32 = 500;
+//pub const DOUBLE_JUMP_POWER_Y: i32 = 130;
+//pub const DODGE_DURATION = 0.13;
+//pub const SLIDE_DURATION = 0.3;
+//pub const SLIDE_DECEL = 100;
+//pub const DODGE_COOLDOWN = 0.13;
+//pub const DODGE_SPEED = 260;
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Player {
@@ -30,24 +57,38 @@ impl Player {
     }
 
     pub fn advance(&mut self, input: u8, level: &Level) {
-        self.velocity.zero();
-        if input & INPUT_UP != 0 && input & INPUT_DOWN == 0 {
-            self.velocity.y = -1777;
+        //self.velocity.zero();
+        //if input & INPUT_UP != 0 && input & INPUT_DOWN == 0 {
+            //self.velocity.y = -1777;
+        //}
+        //if input & INPUT_UP == 0 && input & INPUT_DOWN != 0 {
+            //self.velocity.y = 1777;
+        //}
+        //if input & INPUT_LEFT != 0 && input & INPUT_RIGHT == 0 {
+            //self.velocity.x = -1777;
+            //self.is_facing_right = true;
+        //}
+        //if input & INPUT_LEFT == 0 && input & INPUT_RIGHT != 0 {
+            //self.velocity.x = 1777;
+            //self.is_facing_right = false;
+        //}
+        //let is_on_ground =
+            //self.collide(level, self.hitbox.x, self.hitbox.y + 1);
+
+        if input_check(input, INPUT_LEFT) {
+            self.velocity.x -= RUN_ACCEL / 60;
         }
-        if input & INPUT_UP == 0 && input & INPUT_DOWN != 0 {
-            self.velocity.y = 1777;
+        else if input_check(input, INPUT_RIGHT) {
+            self.velocity.x += RUN_ACCEL / 60;
         }
-        if input & INPUT_LEFT != 0 && input & INPUT_RIGHT == 0 {
-            self.velocity.x = -1777;
-            self.is_facing_right = true;
+        else {
+            self.velocity.x = approach(self.velocity.x, 0, RUN_DECEL);
         }
-        if input & INPUT_LEFT == 0 && input & INPUT_RIGHT != 0 {
-            self.velocity.x = 1777;
-            self.is_facing_right = false;
-        }
+        self.velocity.x = clamp(self.velocity.x, -MAX_RUN_SPEED, MAX_RUN_SPEED);
+
         // TODO: Could optimize by only sweeping
         // when player is at tunneling velocity
-        self.move_by(level, self.velocity.x, self.velocity.y, true);
+        self.move_by(level, self.velocity.x / 60, self.velocity.y / 60, true);
 
         self.current_animation_frame += 1;
         if self.velocity.x != 0 {
--- src/utils.rs
+++ src/utils.rs
@@ -28,3 +28,32 @@ pub fn do_hitboxes_overlap(a: &Hitbox, b: &Hitbox) -> bool {
         || b.y > a.y + a.height;
     return !is_not_overlapping;
 }
+
+pub fn approach(value: i32, target: i32, amount: i32) -> i32 {
+    if value < target - amount {
+        return value + amount;
+    }
+    else if value > target + amount {
+        return value - amount;
+    }
+    else {
+        return target;
+    }
+}
+
+pub fn clamp(value: i32, min: i32, max: i32) -> i32 {
+    if max > min {
+        if value < min { return min; }
+        else if value > max { return max; }
+        else { return value; }
+    }
+    else {
+        if value < max { return max; }
+        else if value > min { return min; }
+        else { return value; }
+    }
+}
+
+pub fn input_check(check: u8, input: u8) -> bool {
+    return input & check != 0;
+}
